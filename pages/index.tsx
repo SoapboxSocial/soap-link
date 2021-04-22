e3354fae9988ce3ff78cfeaf2519f94483b80b58
Jeff Reiner <theundeadghost@gmail.com>
2021-04-22 12:44:51 +0200

    Cleanup template

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Create Next App</h1>
+    </div>
+  );
+}
+
+export default Home;
