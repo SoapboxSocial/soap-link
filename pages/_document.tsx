97d2601d158b36185bc40ba5bcbe1bcb6434f741
Jeff Reiner <theundeadghost@gmail.com>
2021-04-22 13:32:31 +0200

    Adding document

---
+++ pages/_document.tsx
@@ -0,0 +1,54 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class SoapLinkDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" className="text-base sm:text-xl">
+        <Head>
+          {/* Font Preload Tags */}
+          <link
+            rel="preload"
+            href="/SFRounded-Semibold.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/SFRounded-Bold.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
+          {/* Favicon Tags */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="theme-color" content="#8359fe" />
+        </Head>
+
+        <body className="flex flex-col min-h-screen">
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
